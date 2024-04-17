ad9d850f3a384b200d0d731d89be96e148ed905f
Priyanshu Singh <92286906+Priyanshu-5ingh@users.noreply.github.com>
2024-04-17 15:12:55 +0530

    Update minor.js

--- minor.js
+++ minor.js
@@ -1,6 +1,27 @@
-const icon = document.querySelector(".icon");
+var tl= gsap.timeline({
+  scrollTrigger:{
+trigger:".main",
+start:"50% 50%",
+end:"250% 50%",
+scrub:true,
+pin:true,
+// markers:true,
+  }
+})
 
-icon.addEventListener("click", () => {
-  icon.classList.toggle("active");
-  
-});
+tl.to(".svg",{
+  maskSize:"700%"
+},'a');
+tl.to(".img",{
+  backgroundSize:"100%"
+},'a');
+
+const hamMenu=document.querySelector('.ham-menu');
+
+const offscreenmenu=document.querySelector('.off-screen-menu');
+
+hamMenu.addEventListener("click",()=>{
+  hamMenu.classList.toggle('active');
+  offscreenmenu.classList.toggle('active')
+})
+
